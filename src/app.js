c7ad3b9572c40912dc02bff7048817bf6004684d
NicolasArielGerlero <nicogerlero@hotmail.com>
2023-11-16 17:35:37 -0300

    mercadoLiebre 0.0.4 Se mejora el encabezado

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
-app.listen(3000,()=> console.log('Servidor corriendo en el puerto 3000 - http://localhost:3000'));
+app.listen(3030,()=> console.log('Servidor corriendo en el puerto 3030 - http://localhost:3030'));
 
 app.use(express.static('public'));
 

223816ca547ddb075a8ce29374da93410a2c82c7
NicolasArielGerlero <nicogerlero@hotmail.com>
2023-11-21 23:22:33 -0300

    mercadoLiebre 0.0.11 se trabaja en el formulario de registrar

--- src/app.js
+++ src/app.js
@@ -7,5 +7,9 @@ app.listen(3030,()=> console.log('Servidor corriendo en el puerto 3030 - http://
 app.use(express.static('public'));
 
 app.get('/',(req,res)=> {
-    res.sendFile(path.join(__dirname,'/views/home.html'));
+    res.sendFile(path.join(__dirname,'./views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
 });

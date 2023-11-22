9da1641cb8ba585cd0513569941d3c1e76cb4f9a
NicolasArielGerlero <nicogerlero@hotmail.com>
2023-11-22 13:37:10 -0300

    mercadoLiebre 0.0.13 creo la vista de login

--- src/app.js
+++ src/app.js
@@ -13,3 +13,8 @@ app.get('/',(req,res)=> {
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+ 
